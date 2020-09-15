698fac90ea9b651210472d85495272cc61ef4a5c
niztg <65248634+niztg@users.noreply.github.com>
2020-09-15 16:56:16 -0400

    better example

--- test.py
+++ test.py
@@ -1,8 +1,10 @@
 import quranpy
 
-# print(quranpy.Search("abraham", edition=quranpy.Editions.yusufali, surah=quranpy.Surah(21)))
-ikhlas = quranpy.Surah(
-    chapter=quranpy.Chapters.ikhlas,
-    edition=quranpy.Editions.sahih_international
+results = quranpy.Search("abraham", edition=quranpy.Editions.yusufali, surah=quranpy.Surah(21))
+
+nisa = quranpy.Surah(
+    chapter=quranpy.Chapters.nisa,
+    edition=quranpy.Editions.pickthall
 )
-print(ikhlas.verses)
+print(nisa.translation)
+print(results)
