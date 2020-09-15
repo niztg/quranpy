324bd2278c37066590a4f3e6a258b1413f40b775
niztg <65248634+niztg@users.noreply.github.com>
2020-09-15 00:02:48 -0400

    why

---
+++ quranpy/__init__.py
@@ -0,0 +1,7 @@
+from .surah import Surah, Verse
+from .exceptions import SurahNotFound, IncorrectAyahArguments
+from .editions import Editions
+
+__author__ = "niztg"
+__license__ = "MIT"
+
---
+++ quranpy/exceptions.py
@@ -0,0 +1,14 @@
+class SurahNotFound(Exception):
+    def __init__(self, message="That Surah doesn't exist!"):
+        self.message = message
+
+    def __str__(self):
+        return self.message
+
+
+class IncorrectAyahArguments(Exception):
+    def __init__(self, message="That ayah doesn't exist!"):
+        self.message = message
+
+    def __str__(self):
+        return self.message
