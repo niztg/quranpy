644b61f5381a1ca7745f0ed1e1f98e93c2e1f218
niztg <65248634+niztg@users.noreply.github.com>
2020-09-23 00:16:45 -0400

    edition info

--- test.py
+++ test.py
@@ -9,6 +9,8 @@ import quranpy
 with quranpy.edition(quranpy.Editions.sahih_international) as e:
     print(e.verse(262))
 
+print(quranpy.edition(quranpy.Editions.yusufali).english_name)
+
 results = quranpy.Search(
     term="Moses",
     surah=quranpy.Chapters.anbiyaa,
